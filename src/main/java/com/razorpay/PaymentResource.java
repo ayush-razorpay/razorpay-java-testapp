b80c81a8c19319d1f84b5d4e2ba50fd9eb9d47df
Ayush Vipul <ayush.vipul@razorpay.com>
2021-05-04 22:36:18 +0530

    compatible with java - 8 , PaymentResource/charge method can now handle both success and failure callbacks

--- src/main/java/com/razorpay/PaymentResource.java
+++ src/main/java/com/razorpay/PaymentResource.java
@@ -10,14 +10,19 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
 import io.dropwizard.views.View;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Path("/payment")
 public class PaymentResource {
 
+  private ObjectMapper mapper = new ObjectMapper();
   private RazorpayClient client;
   private int amount = 500;
 
@@ -53,27 +58,45 @@ public class PaymentResource {
   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
   @Produces(MediaType.APPLICATION_JSON)
   public Response charge(MultivaluedMap<String, String> formParams) {
+
+
     String paymentId = formParams.getFirst("razorpay_payment_id");
     String razorpaySignature = formParams.getFirst("razorpay_signature");
     String orderId = formParams.getFirst("razorpay_order_id");
+    Map<String,String> toReturn = new HashMap<>();
     JSONObject options = new JSONObject();
 
     if (StringUtils.isNotBlank(paymentId) && StringUtils.isNotBlank(razorpaySignature)
-        && StringUtils.isNotBlank(orderId)) {
+            && StringUtils.isNotBlank(orderId)) {
       try {
         options.put("razorpay_payment_id", paymentId);
         options.put("razorpay_order_id", orderId);
         options.put("razorpay_signature", razorpaySignature);
         boolean isEqual = Utils.verifyPaymentSignature(options, this.secretKey);
 
+        toReturn.put("signature checked","signature validated");
+        toReturn.put("data",formParams.toString());
         if (isEqual) {
-          return Response.ok().build();
+
+        return   Response.ok().entity(toReturn.toString()).build();
         }
+        return Response.status(Status.BAD_REQUEST).build();
       } catch (RazorpayException e) {
         System.out.println("Exception caused because of " + e.getMessage());
         return Response.status(Status.BAD_REQUEST).build();
       }
+    } else {
+      //todo: Add you logic here if needed
+
+     System.out.println("Payment has failed. Processing payment failed callback " );
+     toReturn.put("signature checked","signature not validated");;
+     toReturn.put("data",formParams.toString());
+
+     return Response.ok().entity(toReturn.toString()).build();
+
     }
-    return Response.status(Status.BAD_REQUEST).build();
+
   }
+
+
 }
